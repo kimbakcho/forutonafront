94217062cd1d90fcbe4b2bc2285499a535389560
beckho <vngkgk624@gmail.com>
2019-10-23 01:53:55 +0900

    네이버 로그인 연동 완료

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # forutonafront
 
+A new Flutter project.
 
-<img src="https://github.com/kimbakcho/forutonafront/blob/master/formain.png" width="190" height="100">
+## Getting Started
 
-포로투나 프로젝트 Front
+This project is a starting point for a Flutter application.
 
-<img src="https://github.com/kimbakcho/forutonafront/blob/master/front.png">
+A few resources to get you started if this is your first Flutter project:
 
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/LoginPage/LoginPageView.dart
+++ lib/LoginPage/LoginPageView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
 
 class LoginPageView extends StatefulWidget {
   LoginPageView({Key key}) : super(key: key);
@@ -7,6 +8,9 @@ class LoginPageView extends StatefulWidget {
 }
 
 class _LoginPageViewState extends State<LoginPageView> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +27,101 @@ class _LoginPageViewState extends State<LoginPageView> {
         ),
       ),
       body: Container(
-        child: Text("123"),
+        padding: EdgeInsets.fromLTRB(
+            20, MediaQuery.of(context).size.height * 0.1, 20, 10),
+        child: ListView(
+          children: <Widget>[
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                  hintText: 'ID(Email)',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+            ),
+            Container(
+              height: 10,
+            ),
+            TextField(
+              controller: passController,
+              decoration: InputDecoration(
+                  hintText: 'password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+            ),
+            Container(
+              height: 20,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.09,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)),
+                child: Text(
+                  "Log-in",
+                  style: TextStyle(fontSize: 25),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment(1, 0),
+              child: GestureDetector(
+                child: Text(
+                  "아직 회원이 아니신가요?",
+                  style: TextStyle(
+                      decoration: TextDecoration.underline, color: Colors.blue),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Container(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment(1, 0),
+              child: GestureDetector(
+                child: Text("혹시, 패스워드를 잃어버렸나요?",
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.blue)),
+                onTap: () {},
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            Divider(
+              thickness: 3,
+            ),
+            Container(
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)),
+                child: Text(
+                  "네이버 로그인",
+                  style: TextStyle(fontSize: 25),
+                ),
+                onPressed: () async {
+                  if (await FlutterNaverLogin.isLoggedIn) {
+                    print('login');
+                  } else {
+                    print('no login');
+                  }
+                  NaverLoginResult res1 = await FlutterNaverLogin.logIn();
+                  print(res1.account.id);
+                  print(res1.account.age);
+                  NaverAccessToken res2 =
+                      await FlutterNaverLogin.currentAccessToken;
+                  print(res2.accessToken);
+                  print(res2.expiresAt);
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
