ee151bbfd804f6afe895179fa2c5f7b14034d012
beckho <vngkgk624@gmail.com>
2020-08-27 00:44:11 +0900

    테스트 TODO 리스트 작성

---
+++ test/ICodePage/ID001/ID001WidgetPart/ID001LikeStateViewModel_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:forutonafront/ICodePage/ID001/ID001WidgetPart/ID001LikeState.dart';
+
+void main(){
+  ID001LikeStateViewModel id001likeStateViewModel;
+  setUp((){
+    id001likeStateViewModel = ID001LikeStateViewModel();
+  });
+  test('should', () async {
+    //TODO 테스트 구현 
+    //arrange
+
+    //act
+
+    //assert
+  });
+
+}
