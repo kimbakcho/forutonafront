06718ba15d0a73f697f608f6e4093b5bcaf1be4f
beckho <vngkgk624@gmail.com>
2020-10-11 21:54:50 +0900

    temp

--- lib/KCodePage/K001_01/K00101MainPage.dart
+++ lib/KCodePage/K001_01/K00101MainPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 class K00101MainPage extends StatelessWidget {
   @override
@@ -12,13 +13,63 @@ class K00101MainPage extends StatelessWidget {
               Container(
                 child: Text("123"),
               ),
-              ListView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: model.itemcount,
-                  itemBuilder: (context, index) {
-                    return Container(child: Text("${index}"));
-                  }),
+              StickyHeader(
+                header: Container(
+                  height: 50.0,
+                  color: Colors.blueGrey[700],
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Header #1',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                content: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.itemcount,
+                    itemBuilder: (context, index) {
+                      return Container(child: Text("${index}"));
+                    }),
+              ),
+              StickyHeader(
+                header: Container(
+                  height: 50.0,
+                  color: Colors.blueGrey[700],
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Header #1',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                content: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.itemcount,
+                    itemBuilder: (context, index) {
+                      return Container(child: Text("${index}"));
+                    }),
+              ),
+              StickyHeader(
+                header: Container(
+                  height: 50.0,
+                  color: Colors.blueGrey[700],
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Header #2',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                content: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.itemcount,
+                    itemBuilder: (context, index) {
+                      return Container(child: Text("${index}"));
+                    }),
+              ),
               FlatButton(
                 onPressed: model.itemcoutplus,
                 child: Text("123"),
@@ -30,10 +81,9 @@ class K00101MainPage extends StatelessWidget {
 }
 
 class K00101MainPageViewModel extends ChangeNotifier {
-
   int itemcount = 1;
 
-  void itemcoutplus(){
+  void itemcoutplus() {
     itemcount++;
     notifyListeners();
   }
